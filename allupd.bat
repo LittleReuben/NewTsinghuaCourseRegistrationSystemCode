ae1d183431d76086af487e8138a1f8d5a5836788
qAlex-Weiq <1826980180@qq.com>
2025-07-04 16:23:54 +0800

    fix the failure of deleteing course and course_group

--- allupd.bat
+++ allupd.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 REM 设置你的 Git 操作参数
-set /p COMMIT_MESSAGE="请输入提交信息: "
+set /p COMMIT_MESSAGE="fix the failure of deleting course and course_group"
 
 REM 遍历当前文件夹下的所有子文件夹
 for /D %%d in (*) do (
